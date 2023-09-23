f818fa3ac1753a2f3b9d5ccf4564033fede5ba6d
leonardo-gennaioli <leonardo.gennaioli@studio.unibo.it>
2023-09-23 16:00:16 +0200

    daje

--- src/beans/Società.java
+++ src/beans/Società.java
@@ -9,6 +9,39 @@ public class Società {
 	private ArrayList<Squadra> squadre;
 	private String nome;
 	private Calendario calendario;
+	public Società(String nome) {
+		this.nome = nome;
+		this.palestre= new ArrayList<Palestra>();
+		this.squadre= new ArrayList<Squadra>();
+	}
+	public ArrayList<Palestra> getPalestre() {
+		return palestre;
+	}
+	public void setPalestre(ArrayList<Palestra> palestre) {
+		this.palestre = palestre;
+	}
+	public ArrayList<Squadra> getSquadre() {
+		return squadre;
+	}
+	public void setSquadre(ArrayList<Squadra> squadre) {
+		this.squadre = squadre;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Calendario getCalendario() {
+		return calendario;
+	}
+	public void setCalendario(Calendario calendario) {
+		this.calendario = calendario;
+	}
+	
+	
+	
+	
 	
 	
 }
