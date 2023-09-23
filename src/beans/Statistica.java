91a12a698e222034a294a43b3229293a4b3fa62c
lorenzo-severini <lorenzo20112001@live.it>
2023-09-23 15:53:03 +0200

    commit

---
+++ src/beans/Statistica.java
@@ -0,0 +1,29 @@
+package beans;
+
+import javax.servlet.http.HttpSession;
+
+public class Statistica {
+
+	private int min;
+	private int ast;
+	private int pts;
+	private int fgm;
+	private int fga;
+	private double fg_perc;
+	private int three_pm;
+	private int three_pa;
+	private double three_p_perc;
+	private int two_pm;
+	private int two_pa;
+	private double two_p_perc;
+	private int ftm;
+	private int fta;
+	private double ft_perc;
+	private int blk;
+	private int stl;
+	private int tov;
+	private int pf;
+	
+	
+	
+}
