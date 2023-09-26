a299f0c97ae4a13291086579f919824b9624d81e
Matteo Bostrenghi <mattiyugi@gmail.com>
2023-09-26 22:06:18 +0200

    porcodio

--- src/servlet/MostraRichieste.java
+++ src/servlet/MostraRichieste.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 
 import beans.Calendario;
 import beans.Giocatore;
+import beans.Richiesta;
 import beans.Societa;
 import beans.Squadra;
 
@@ -35,7 +36,7 @@ public class MostraRichieste extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
-		ArrayList Richieste=new ArraListreq.getServletContext().getAttribute("richieste");
+		ArrayList<Richiesta> richieste=new ArrayList<>(this.getServletContext().getAttribute("richieste"));
 	}
 	
 	
