3040322d5b61478575e0fe4fd1c721f1b7a22799
leonardo-gennaioli <leonardo.gennaioli@studio.unibo.it>
2023-09-27 01:03:37 +0200

    hh

--- src/beans/Calendario.java
+++ src/beans/Calendario.java
@@ -14,30 +14,26 @@ public class Calendario {
         this.impegni.add(i);
     }
     public void remove(String idImpegno){
-        
+       for(Impegno i: this.impegni)
+       {
+    	   if(i.getIdImpegno().equals(idImpegno))
+    		   this.impegni.remove(i);
+       }
     }
 
-    public Impegno getImpegno(String id){
-        for (int i=0; i<giorni.size();i++){
-            if(giorni.get(i).getIdImpegno().compareTo(id)==0){
-                return giorni.get(i);
-            }
+    public Impegno getImpegno(String idImpegno){
+    	for(Impegno i: this.impegni)
+        {
+     	   if(i.getIdImpegno().equals(idImpegno))
+     		   return i;
         }
         return null;
 
     }
-    public List<Impegno> selectMese(String mese){
-        ArrayList<Impegno> res= new ArrayList<>();
-        for (int i=0; i<giorni.size();i++){
-            if(giorni.get(i).getInizio().getMonth().toString().compareTo(mese)==0){
-                res.add(giorni.get(i));
-            }
-        }
-        return res;
-    }
-    public  List<Impegno> getImpegniSquadra(){
+   
+    public  List<Impegno> getImpegni(){
         
-        return this.giorni;
+        return this.impegni;
     }
 
 }
